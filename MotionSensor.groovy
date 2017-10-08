e8e3a733d1265758ebb5acbcb752231c0154edd2
Darren Murray <dmurray1701@gmail.com>
2017-10-08 18:38:24 +0100

    Initial Motion Sensor test

---
+++ MotionSensor.groovy
@@ -0,0 +1,39 @@
+definition(
+    name: "Motion Sensor Test",
+    namespace: "DarrenMurray",
+    author: "Darren Murray",
+    description: "Test App",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("Select motion sensors") {
+        input("sensors", "capability.motionSensor",
+                          required: true, multiple: true)
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// Subscribe to attributes, devices, locations, etc.
+    subscribe(sensors, "motion", evtHandlerMotion)
+}
+
+//  Event handlers
+def evtHandlerMotion(evt) {
+    log.debug("Motion Event Handler called")
+}
